6ff56e105e09cd31ad6d419363c43c376707037e
Matheus Kauan Passos De Souza <matheus.kp.souza@gmail.com>
2022-10-10 20:49:50 -0300

    Update TAD_Palavra.h
    Reforma no código :+1:

--- código/Headers/TAD_Palavra.h
+++ código/Headers/TAD_Palavra.h
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "TAD_Lista_De_Posicoes.h"
 
 typedef struct{
     char *Palavra;
+    TLista_De_Posicoes *posicoes;
 }TPalavra;
 
 /*Criação e preenchimento da cadeia de caracteres*/
-void Criar_Palavra_Vazia(TPalavra* pPalavra, int n);
-void Preenche_Cadeia_De_Caracteres(TPalavra* pPalavra, FILE *file); /* SET */
+void FPosicao_Vazia(TPalavra* pPalavra);
+void Preenche_Cadeia_De_Caracteres(TPalavra* pPalavra, char *ppalavra, int tamanho); /* SET */
 
 /*....*/
-void Retorna_Cadeia_De_Caracteres(TPalavra* pPalavra, FILE *file, char *palavra); /* GET */
+char* Retorna_Cadeia_De_Caracteres(TPalavra* pPalavra); /* GET */
 void Imprime_Cadeia_De_Caracteres(TPalavra* pPalavra);
-void Imprime_Palavra(TPalavra* pPalavra);
+void Imprime_Palavra(TPalavra* pPalavra, TLista_De_Posicoes *posicoes);
