cbffdf96d178a69d314dfe310d0ebb454875752e
Matheus Kauan Passos De Souza <105021005+Matheuskpds@users.noreply.github.com>
2022-10-11 11:31:02 -0300

    Update and rename TAD_Lista_De_Posicoes.c to TAD_ListaDePosicoes.c

--- código/Source/TAD_ListaDePosicoes.c
+++ código/Source/TAD_ListaDePosicoes.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include"TAD_Posicao.h"
+#include"TAD_ListaDePosicoes.h"
 
 void FLPosicoes_Vazia(TLista_De_Posicoes* pLista){
 
@@ -24,12 +24,14 @@ void Imprimir_Posicao(TLista_De_Posicoes* pLista){
 
 	CPosicao* pAux;
     pAux = pLista->pPrimeiro->pProx;
+    printf("\n");
     while(pAux != NULL) {
     
         printf("%d ", pAux->posicao);
         pAux = pAux->pProx;
     
     }
+    printf("\n");
 
 }
 
