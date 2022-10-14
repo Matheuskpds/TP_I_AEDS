00964403c7c79646811078104dc5902ef27ec88b
Esdraas <114784438+Esdraas@users.noreply.github.com>
2022-10-14 18:42:56 -0300

    Update TAD_Dicionario.h

--- código/Headers/TAD_Dicionario.h
+++ código/Headers/TAD_Dicionario.h
@@ -5,7 +5,8 @@
 typedef struct CelulaDicionario* ApontadorDicionario;
 //celula da lista
 typedef struct CelulaDicionario{
-    TLista_De_Palavras listaDePalavras;
+    char letra_lista;
+    TLista_De_Palavras ListaDePalavras;
     struct CelulaDicionario* pProx;
 }TCelulaDicionario;
 //lista de listas - dicionario
@@ -18,4 +19,8 @@ typedef struct{
 //operacoes do dicionario
 void Inicializa_Dicionario(TDicionario* pDicionario);
 void Constroi_Dicionario(TDicionario* pDicionario);
+void Cria_e_Insere_Nova_Lista(TDicionario* pDicionario, char letra);
+ApontadorDicionario Verifica_Lista(TDicionario* pDicionario, char letra);
+int Dicionario_e_vazio(TDicionario* pDicionario);
+void Ordena_Dicionario(TDicionario* pDicionario);
 
