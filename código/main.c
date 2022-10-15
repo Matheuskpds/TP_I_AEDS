22d124bd82bd0e0878868d9ffa9fd70e55f3fcb7
Matheus Kauan Passos De Souza <105021005+Matheuskpds@users.noreply.github.com>
2022-10-15 13:32:51 -0300

    Update main.c

--- código/main.c
+++ código/main.c
@@ -5,7 +5,7 @@
 void opcoes(){
     printf("-=-=-=-=-=-=-=-=-=-=-=-= MENU -=-=-=-=-=-=-=-=-=-=-=\n");
 
-    printf("[1] Inicializar com arquivo txt\n");
+    printf("[1] Contruir Dicionario\n");
     printf("[2] Imprimir Dicionario\n");
     printf("[3] Imprimir Dicionario em Ordem Alfabética\n");
     printf("[4] Remover Ultima Palavra\n");
@@ -19,21 +19,26 @@ void opcoes(){
 void menu(){
     TLista_De_Palavras pListaDePalavras;
     TDicionario pDicionario;
+    FILE* arquivo = NULL;
 
-
-    int resp;
+    int resp = 0;
     char enter;
     int loop=1;
     char deletar_palavra[50] = {0};
     Inicializa_Dicionario(&pDicionario);
-    //while(loop){
-        /*opcoes();
-        scanf("%d", &resp);
+    
+    char nomeArquivo[100]={0};
+    
+    while(loop){
+        opcoes();
         fflush(stdin);
-        getchar();*/
-        /*switch(resp){
+        scanf("%d", &resp);
+        switch(resp){
             case 1:
-                Constroi_Dicionario(&pDicionario);
+                printf("Insira o nome do arquivo de entrada: \n");
+                scanf("%[^\n]", nomeArquivo);
+                arquivo=fopen(nomeArquivo,"rt");
+                Constroi_Dicionario(&pDicionario, arquivo);
                 break;
             case 2:
                 //Imprimir palavras com posicoes
@@ -41,7 +46,7 @@ void menu(){
                 break;
             case 3:
                 //Aguardando a função ser criada
-            /*case 4:
+            case 4:
                 Remove_Palavra_Final(&pListaDePalavras);
                 break;
             case 5:
@@ -54,13 +59,12 @@ void menu(){
                 break;
         }
         if(resp!=6){
+            fflush(stdin);
             printf("Aperte enter para continuar.//");
+            getchar();
             printf("\n");
-            scanf("%c", &enter);
-        }*/
-    //}
-    Constroi_Dicionario(&pDicionario);
-    Imprime_Dicionario(pDicionario);
+        }
+    }
       
 }
 
